030ba3269a78a69664ff45869c6a0ccb94b3307c
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-17 19:09:47 -0300

    User Interface created

--- app/src/main/java/com/projects/implicitintents/MainActivity.java
+++ app/src/main/java/com/projects/implicitintents/MainActivity.java
@@ -3,6 +3,7 @@ package com.projects.implicitintents;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+    }
+
+    public void openLocation(View view) {
+    }
+
+    public void shareText(View view) {
     }
 }
