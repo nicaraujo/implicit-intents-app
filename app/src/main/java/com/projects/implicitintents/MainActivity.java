27f3b545ed20e89ee27a9da7166fee1ebdcbb9be
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-03-17 20:07:28 -0300

    Open website, location and share buttons implemented

--- app/src/main/java/com/projects/implicitintents/MainActivity.java
+++ app/src/main/java/com/projects/implicitintents/MainActivity.java
@@ -1,24 +1,55 @@
 package com.projects.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ShareCompat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText website;
+    private EditText location;
+    private EditText share;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        website = findViewById(R.id.website_edittext);
+        location = findViewById(R.id.location_edittext);
+        share = findViewById(R.id.share_edittext);
     }
 
     public void openWebsite(View view) {
+        String url = website.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this");
+        }
     }
 
     public void openLocation(View view) {
+        String address = location.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + address);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this");
+        }
     }
 
     public void shareText(View view) {
+        String text = share.getText().toString();
+        String mimeType = "text/plain";
+        ShareCompat.IntentBuilder.from(this).setType(mimeType).setChooserTitle(R.string.share_text).setText(text).startChooser();
     }
 }
